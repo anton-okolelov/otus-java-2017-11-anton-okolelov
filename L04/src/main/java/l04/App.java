0ab62044e07c27c2190160affd6044461b2231e7
Anton O <noname@noemail>
2017-12-18 17:43:39 +0300

    l04

---
+++ L04/src/main/java/l04/App.java
@@ -0,0 +1,13 @@
+package l04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        while (true) {
+            list.add(1);
+        }
+    }
+}
